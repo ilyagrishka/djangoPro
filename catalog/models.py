7fd1f70e6cb85f393753fa86fd981685cc1373bd
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-06-03 20:15:14 +0400

    add

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название продукта",
+        help_text="Введите название продукта",
+    )
+    description = models.TextField(
+        max_length=100,
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+    )
+    photo = models.ImageField(
+        upload_to="product/photo",
+        verbose_name="Фото",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.ForeignKey("Category",
+                                 on_delete=models.SET_NULL, blank=True, null=True, related_name="products"
+                                 )
+    price = models.IntegerField(
+        verbose_name="Цена продукта", help_text="Введите цену продукта"
+    )
+    date_create = models.DateTimeField(
+        blank=True, verbose_name="Дата создания", help_text="Введите дату загрузки"
+    )
+    date_last_change = models.DateTimeField(
+        blank=True, verbose_name="Дата последнего изменения", help_text="Введите дату"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "price"]
+
+    def __str__(self):
+        return f"{self.name}-{self.description}"
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название категории",
+        help_text="Введите название категории",
+    )
+    description = models.TextField(
+        max_length=100,
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}-{self.description}"
