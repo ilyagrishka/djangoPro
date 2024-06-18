e720d6f0aeb1c35129d02444ca7081452ca57ff2
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-06-18 20:25:43 +0400

    more more

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from catalog.models import Product
+from catalog.models import Product, BlogNote
 from django.urls import reverse_lazy
 
 
@@ -35,3 +35,38 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
+
+
+class BlogNoteListView(ListView):
+    model = BlogNote
+
+
+class BlogNoteDetail(DetailView):
+    model = BlogNote
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
+
+class BlogNoteCreate(CreateView):
+    model = BlogNote
+    fields = ("name", "description",)
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class BlogNoteUpdateView(UpdateView):
+    model = BlogNote
+    fields = ("name", "description",)
+    success_url = reverse_lazy("catalog:product_list")
+
+    def get_success_url(self):
+        return reverse_lazy("catalog:products_detail", args=[self.kwargs.get("pk")])
+
+
+class BlogNoteDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:product_list")
+
