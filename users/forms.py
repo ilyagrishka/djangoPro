56315e3fae59b2425554ecf607c93657c19dd340
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-07-22 17:59:49 +0400

    new1

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,7 @@ from catalog.forms import StyleFormMixin
 from users.models import User
 
 
-
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
-
-
-
-
-
